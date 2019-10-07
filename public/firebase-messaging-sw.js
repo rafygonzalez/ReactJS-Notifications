d6719128c9464d089892b5d51ac082d10b2a6c05
rafygonzalez <rafygonzalez089@gmail.com>
2019-10-07 15:01:48 -0400

    Commented

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,17 +5,10 @@ importScripts("https://www.gstatic.com/firebasejs/6.2.0/firebase-messaging.js");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-
+/* CONFIG
 const config = {
-  apiKey: "AIzaSyD8nq1lTBsCOENum6BwGD_pDmm_71cg4Pg",
-  authDomain: "vcinos.firebaseapp.com",
-  databaseURL: "https://vcinos.firebaseio.com",
-  projectId: "vcinos",
-  storageBucket: "vcinos.appspot.com",
-  messagingSenderId: "182898028792",
-  appId: "1:182898028792:web:70cb293be45f6f0f670b0d",
-  measurementId: "G-CTVMH12QZK"
-};
+ 
+};*/
 
 const db = !firebase.apps[0] && firebase.initializeApp(config);
 console.log(firebase.apps);
